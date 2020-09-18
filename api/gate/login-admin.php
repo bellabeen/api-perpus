baeafd9f75d07d1de6e48d9e5a015d6f2a06469e
bellabeen <rootsquid@gmail.com>
2020-09-18 14:00:49 +0700

    new login

---
+++ api/gate/login-admin.php
@@ -0,0 +1,48 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+  
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/login.php';
+  
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+  
+// prepare product object
+$login = new Login($db);
+
+//set ID property
+$login->username = isset($_GET['username']) ? $_GET['username'] : die();
+$login->password = md5(isset($_GET['password']) ? $_GET['password'] : die());
+
+$stmt = $login->loginAdmin();
+if($stmt->rowCount() > 0) {
+    // get retrieved row
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // create array
+    $login_arr = array(
+        "status" => true,
+        "message" => "Successfully Login!",
+        "username" => $row['username'],
+        "nama" => $row['nama']
+    );
+} else {
+    $login_arr = array (
+        "status" => false,
+        "message" => "Invalid Username or Password",
+    );
+}
+
+// make it json format
+// echo json_encode($login_arr);
+print_r(json_encode($login_arr));
+
+
+?>
--- api/objects/login.php
+++ api/objects/login.php
@@ -4,6 +4,7 @@ class Login{
     // database connection and table name
     private $conn;
     private $table_name = "anggota";
+    private $table_name1 = "user";
   
     // object properties
     public $indeks;
@@ -16,6 +17,11 @@ class Login{
     public $id_sesi;
     public $lastlogin;
     public $host;
+    public $id_user;
+    public $username;
+    public $password;
+    public $nama;
+    
   
     // constructor with $db as database connection
     public function __construct($db){
@@ -31,6 +37,22 @@ class Login{
                     " . $this->table_name . " 
                 WHERE
                     indeks='".$this->indeks."' AND kata_sandi='".$this->kata_sandi."'";
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+        // execute query
+        $stmt->execute();
+        return $stmt;
+    }
+
+    //login admin
+    function loginAdmin(){
+        // select all query
+        $query = "SELECT
+                    *
+                FROM
+                    " . $this->table_name1 . " 
+                WHERE
+                    username='".$this->username."' AND password='".$this->password."'";
         // prepare query statement
         $stmt = $this->conn->prepare($query);
         // execute query
